7593a659f5ea9f4584f1e9900855637e017ff113
Alex <sun123vet@gmail.com>
2021-12-06 15:55:24 +0700

    Add SQL database file

---
+++ model/CoffeeShop.sql
@@ -0,0 +1,71 @@
+﻿CREATE DATABASE CoffeeShop
+GO
+
+USE CoffeeShop
+GO
+
+-- Menu
+-- Table
+-- Categories
+-- Account
+-- Receipt
+-- ReceiptInfo
+
+CREATE TABLE CoffeeTable
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'No named',
+	tablesStatus NVARCHAR(100) DEFAULT N'Trống' NOT NULL
+)
+GO
+
+CREATE TABLE Account
+(
+	userName NVARCHAR(100) PRIMARY KEY,
+	displayName NVARCHAR(100) NOT NULL DEFAULT N'No named',
+	password NVARCHAR(1000) NOT NULL DEFAULT 0,
+	accountType INT NOT NULL DEFAULT 0
+)
+GO
+
+CREATE TABLE Categories
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'No named',
+)
+GO
+
+CREATE TABLE Menu
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'No named',
+	idCategories INT NOT NULL,
+	price INT NOT NULL DEFAULT 0,
+
+	FOREIGN KEY (idCategories) REFERENCES dbo.Categories(id)
+)
+GO
+
+CREATE TABLE Receipt
+(
+	id INT IDENTITY PRIMARY KEY,
+	arrivalDate DATETIME NOT NULL DEFAULT GETDATE(),
+	departDate DATETIME,
+	idTable INT NOT NULL,
+	STATUS INT NOT NULL DEFAULT 0,
+
+	FOREIGN KEY (idTable) REFERENCES dbo.CoffeeTable(id)
+)
+GO
+
+CREATE TABLE ReceiptInfo
+(
+	id INT IDENTITY PRIMARY KEY,
+	idReceipt INT NOT NULL,
+	idMenu INT NOT NULL,
+	numberOfFood INT NOT NULL DEFAULT 0,
+
+	FOREIGN KEY (idReceipt) REFERENCES dbo.Receipt(id),
+	FOREIGN KEY (idMenu) REFERENCES dbo.Menu(id)
+)
+GO
